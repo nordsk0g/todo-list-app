292e7cca082b1615da049958a824648cc8f46162
Steven <stevenchristian@outlook.com.au>
2019-12-29 20:49:17 +1100

    Updates

---
+++ precache-manifest.744fbaa629cbfe850cacaf1eaa5dd6fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1471a3c4f52cf748a23e65361f0132f3",
+    "url": "/todo-list-app/index.html"
+  },
+  {
+    "revision": "0cc39a0fdc3e82c07629",
+    "url": "/todo-list-app/static/css/main.a578be7b.chunk.css"
+  },
+  {
+    "revision": "96b96886bcc4881b48df",
+    "url": "/todo-list-app/static/js/2.003b46f7.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/todo-list-app/static/js/2.003b46f7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0cc39a0fdc3e82c07629",
+    "url": "/todo-list-app/static/js/main.2a8bd9fc.chunk.js"
+  },
+  {
+    "revision": "f455908d176aa6fec19f",
+    "url": "/todo-list-app/static/js/runtime-main.b7a4ed14.js"
+  }
+]);
